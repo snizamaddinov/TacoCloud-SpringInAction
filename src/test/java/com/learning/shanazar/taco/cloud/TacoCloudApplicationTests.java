c250caeaa09f33024f7e7066245c0cc7ba2b6b8e
Shanazar <snizamaddinow@gmail.com>
2024-06-18 13:46:28 +0300

    initial commit

---
+++ src/test/java/com/learning/shanazar/taco/cloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.shanazar.taco.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
